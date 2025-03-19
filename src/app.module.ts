e7e347fdd1ee5772ad16b285cb5f4f947ae45d25
shimfff <shimseawon1510@gmail.com>
2025-03-19 12:44:58 +0900

    :sparkles: feat: 루트 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenAIModule } from './openai/openai.module';
+import { ChatModule } from './chat/chat.module';
+import { KakaoModule } from './kakao/kakao.module';
 
 @Module({
-  imports: [],
+  imports: [OpenAIModule, ChatModule, KakaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
