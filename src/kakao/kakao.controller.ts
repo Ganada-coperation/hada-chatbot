311ef8c5037bb4ae35f1b73c3cc78527348d8d6f
shimfff <shimseawon1510@gmail.com>
2025-03-19 16:56:12 +0900

    :sparkles: feat: 요청 dto 생성

--- src/kakao/kakao.controller.ts
+++ src/kakao/kakao.controller.ts
@@ -1,13 +1,16 @@
 import {Controller, Post, Body} from '@nestjs/common';
 import { ChatService } from '../chat/chat.service';
+import { SkillPayloadDto } from './kakao.dto';
+import {ApiBody} from "@nestjs/swagger";
 
 @Controller('kakao')
 export class KakaoController {
     constructor(private readonly chatService: ChatService) {}
 
     // 카카오톡 챗봇 API
+    @ApiBody({ type: SkillPayloadDto })
     @Post('chat')
-    async receiveMessage(@Body() body: any): Promise<any> {
+    async receiveMessage(@Body() body: SkillPayloadDto): Promise<any> {
         const userMessage = body.userRequest.utterance;
         const userId = body.userRequest.user.id;
 
@@ -16,11 +19,14 @@ export class KakaoController {
             userSessions[userId] = [];
         }
 
+        // 이전 대화 내역 조회
+        const chatHistory = userSessions[userId].join("\n");
+
         // 세션에 사용자 메시지 저장
         userSessions[userId].push(userMessage);
 
         // 대화 기능 (ChatGPT 응답)
-        const gptResponse = await this.chatService.chatWithGPT(userMessage);
+        const gptResponse = await this.chatService.chatWithGPT(chatHistory, userMessage);
         userSessions[userId].push(gptResponse);
 
         // 카카오톡 응답 JSON 형식 변환
@@ -28,8 +34,9 @@ export class KakaoController {
     }
 
     // 채팅 바탕 글 생성 API
+    @ApiBody({ type: SkillPayloadDto })
     @Post('create-article')
-    async createArticle(@Body() body: any): Promise<any> {
+    async createArticle(@Body() body: SkillPayloadDto): Promise<any> {
         const userId = body.userRequest.user.id;
 
         // 사용자별 세션 저장소에 사용자 ID가 없으면 새로운 세션을 생성
@@ -49,8 +56,9 @@ export class KakaoController {
     }
 
     // 채팅 끝내기 (이전 채팅 내역 삭제) todo 일단 POST로 구현
+    @ApiBody({ type: SkillPayloadDto })
     @Post('delete-chat')
-    async deleteChat(@Body() body: any): Promise<any> {
+    async deleteChat(@Body() body: SkillPayloadDto): Promise<any> {
         const userId = body.userRequest.user.id;
 
         // 사용자별 세션 저장소에 사용자 ID가 없으면 새로운 세션을 생성
@@ -61,7 +69,7 @@ export class KakaoController {
         // 이전 대화 내역 삭제
         delete userSessions[userId];
 
-        // 카카오톡 응답 JSON 형식 변환
+        // 카카오톡 응답 JSON 형식 변환 todo : 안내 메시지 바꾸기
         return this.formatKakaoResponse("하다와 대화가 종료되었어요! 다시 대화를 시작하려면 \"하다야\"와 함께 메시지를 입력해주세요.");
     }
 
