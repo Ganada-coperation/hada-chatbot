61fe93b6874c6a1a6968fbb69c09fd32e8702d6c
shimfff <shimseawon1510@gmail.com>
2025-03-20 15:06:39 +0900

    :sparkles: feat: 이전 디비 연결 정보 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
+import {Logger, Module, OnApplicationBootstrap} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenAIModule } from './domain/openai/openai.module';
 import { ChatModule } from './domain/chat/chat.module';
 import { KakaoModule } from './domain/kakao/kakao.module';
 import { configValidationSchema } from './config/validation/config-validation';
-import databaseConfig from './config/database.config';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -15,7 +14,6 @@ import { MongooseModule } from '@nestjs/mongoose';
     cache: true,
     envFilePath: [ '.env.' + process.env.NODE_ENV ], // 실행 환경에 따라 .env 파일을 선택
     validationSchema: configValidationSchema,
-    load: [ databaseConfig ],
   }),
     // MongoDB 연결
     MongooseModule.forRootAsync({
--- src/config/validation/config-validation.ts
+++ src/config/validation/config-validation.ts
@@ -2,10 +2,6 @@ import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
     // DATABASE
-    DB_CONNECTION: Joi.string().min(1).required(),
     DB_HOST: Joi.string().min(1).required(),
-    DB_PORT: Joi.string().min(1).required(),
-    DB_DATABASE: Joi.string().min(1).required(),
-    DB_USERNAME: Joi.string().min(1).required(),
-    DB_PASSWORD: Joi.string().optional(),
+    OPENAI_API_KEY: Joi.string().min(1).required()
 });
