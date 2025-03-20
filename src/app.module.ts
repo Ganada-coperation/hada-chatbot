1eb8ba8f61268fe9c7028e8d550826ef153d6f8c
shimfff <shimseawon1510@gmail.com>
2025-03-20 12:42:08 +0900

    :truck: rename: 패키지 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OpenAIModule } from './openai/openai.module';
-import { ChatModule } from './chat/chat.module';
-import { KakaoModule } from './kakao/kakao.module';
+import { OpenAIModule } from './domain/openai/openai.module';
+import { ChatModule } from './domain/chat/chat.module';
+import { KakaoModule } from './domain/kakao/kakao.module';
 import { configValidationSchema } from './config/validation/config-validation';
 import databaseConfig from './config/database.config';
 import {ConfigModule} from "@nestjs/config";
