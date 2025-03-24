16aad47830088b6849462d1fa5b7b2954efb0e68
shimfff <shimseawon1510@gmail.com>
2025-03-24 19:29:53 +0900

    :sparkles: feat: HttpModule 추가

--- src/domain/kakao/kakao.module.ts
+++ src/domain/kakao/kakao.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KakaoController } from './kakao.controller';
 import { ChatModule } from '../chat/chat.module';
+import {HttpModule} from "@nestjs/axios";
 
 @Module({
-    imports: [ChatModule],
+    imports: [ChatModule, HttpModule],
     controllers: [KakaoController],
 })
 export class KakaoModule {}
