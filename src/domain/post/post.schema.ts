479992708ccedeae4d9a110c2108df9a3daa39a2
shimfff <shimseawon1510@gmail.com>
2025-04-04 16:40:41 +0900

    :sparkles: feat : 베이스 스키마 옵셔널 삭제

--- src/domain/post/post.schema.ts
+++ src/domain/post/post.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import {Document, HydratedDocument} from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
-import {BaseSchema} from "../../common/base/base.enetity";
+import {BaseSchema} from "../../common/base/base.schema";
 
 @Schema()
 export class Post extends BaseSchema {
