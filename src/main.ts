1d3ceb1dc6207b5c26c2a396a19235c1c92d507b
shimfff <shimseawon1510@gmail.com>
2025-04-04 14:34:14 +0900

    :recycle: refactor: ValidationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,6 +27,15 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   app.useGlobalFilters(); // Express의 JSON 응답을 유지하도록 설정
+
+  // ValidationPipe 추가 (decorator를 사용한 유효성 검사)
+  app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        transformOptions: { enableImplicitConversion: true },
+      }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
