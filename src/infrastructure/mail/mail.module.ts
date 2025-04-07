de6fcdefd3e4365a7deb135ad33736e67af55269
shimfff <shimseawon1510@gmail.com>
2025-04-07 20:01:38 +0900

    :sparkles: feat : 메일 템플릿 추가

--- src/infrastructure/mail/mail.module.ts
+++ src/infrastructure/mail/mail.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
 import {ConfigModule, ConfigService} from "@nestjs/config";
+import {HandlebarsAdapter} from "@nestjs-modules/mailer/dist/adapters/handlebars.adapter";
+import * as path from "node:path";
 
 @Module({
     imports: [
-        ConfigModule, // ✅ ConfigModule 추가!
+        ConfigModule, // ConfigModule 추가!
         MailerModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
@@ -20,10 +22,17 @@ import {ConfigModule, ConfigService} from "@nestjs/config";
                 defaults: {
                     from: '"Hada 서비스" <no-reply@hada.com>',
                 },
+                template: {
+                    dir: path.resolve(process.cwd(), 'src', 'infrastructure', 'mail', 'templates'), // 템플릿 폴더 경로
+                    adapter: new HandlebarsAdapter(), // ✍사용할 템플릿 엔진 (Handlebars 권장)
+                    options: {
+                        strict: true,
+                    },
+                },
             }),
         }),
     ],
     providers: [MailService],
-    exports: [MailService], // ✅ 다른 모듈에서 사용할 수 있도록 export
+    exports: [MailService], // 다른 모듈에서 사용할 수 있도록 export
 })
 export class MailModule {}
