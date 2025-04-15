6d15a2ffa244fc164459905a37d48b26eccd8e61
shimfff <shimseawon1510@gmail.com>
2025-04-15 17:54:20 +0900

    :sparkles: feat: 컨트롤러 묘듈 등록

--- src/domain/chat/chat.module.ts
+++ src/domain/chat/chat.module.ts
@@ -8,12 +8,14 @@ import {ChatMessage, ChatMessageSchema} from "./schema/chat.message.schema";
 import {ChatSession, ChatSessionSchema} from "./schema/chat.session.schema";
 import {GeneratedContent, GeneratedContentSchema} from "./schema/generated.content.schema";
 import {GeneratedContentService} from "./service/generated.content.service";
+import {GeneratedContentController} from "./generated.content.controller";
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: ChatMessage.name, schema: ChatMessageSchema }
         , {name: ChatSession.name, schema: ChatSessionSchema}
         , {name: GeneratedContent.name, schema: GeneratedContentSchema}])
         , OpenAIModule],
+    controllers: [GeneratedContentController],
     providers: [ChatService, ChatDataService, GeneratedContentService],
     exports: [ChatService, ChatDataService, GeneratedContentService],
 })
