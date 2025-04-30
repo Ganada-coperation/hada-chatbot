74fdc92f9908f03d65debe9aeb96548638398b41
shimfff <shimseawon1510@gmail.com>
2025-04-30 18:49:53 +0900

    :sparkles: feat: 레디스 비번 추가

--- src/config/bull.config.ts
+++ src/config/bull.config.ts
@@ -1,9 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 
-export const bullConfig = (configService: ConfigService) => ({
-    redis: {
-        host: configService.get<string>('REDIS_HOST'),
-        port: configService.get<number>('REDIS_PORT'),
-        // password: configService.get<string>('REDIS_PASSWORD'), // 필요시
-    },
-});
+export const bullConfig = (configService: ConfigService) => {
+    const redisHost = configService.get<string>('REDIS_HOST');
+    const redisPort = configService.get<number>('REDIS_PORT');
+    const redisPassword = configService.get<string>('REDIS_PASSWORD');
+
+    console.log('Redis password in use:', redisPassword);
+
+    return {
+        redis: {
+            host: redisHost,
+            port: redisPort,
+            password: redisPassword,
+        },
+    };
+};
