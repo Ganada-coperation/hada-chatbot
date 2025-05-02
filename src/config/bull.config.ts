41728b362ed34082282baa0d4c95eff545c99360
shimfff <shimseawon1510@gmail.com>
2025-05-02 11:04:57 +0900

    :sparkles: feat: 레디스 설정 변경

--- src/config/bull.config.ts
+++ src/config/bull.config.ts
@@ -3,15 +3,15 @@ import { ConfigService } from '@nestjs/config';
 export const bullConfig = (configService: ConfigService) => {
     const redisHost = configService.get<string>('REDIS_HOST');
     const redisPort = configService.get<number>('REDIS_PORT');
-    const redisPassword = configService.get<string>('REDIS_PASSWORD');
+    // const redisPassword = configService.get<string>('REDIS_PASSWORD');
 
-    console.log('Redis password in use:', redisPassword);
+    // console.log('Redis password in use:', redisPassword);
 
     return {
         redis: {
             host: redisHost,
             port: redisPort,
-            password: redisPassword,
+            // password: redisPassword,
         },
     };
 };
